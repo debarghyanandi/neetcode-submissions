b7f9fedf9890ded9a54411c5b2b9b6f406fab49a
Debarghya <debarghyanandi4@gmail.com>
2026-08-10 12:32:08 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+
+       HashSet<int> seen = new HashSet<int>();
+       foreach (var n in nums){
+        if (!seen.Add(n))
+            return true;
+       }
+       return false;
+    }
+}
