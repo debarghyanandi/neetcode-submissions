b4ac68fdac1da69f012f524e1fe5598d7ac8b1c9
Debarghya <debarghyanandi4@gmail.com>
2026-08-26 06:36:31 +0530

    Update submission-6.cs

--- Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cs
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cs
@@ -10,7 +10,9 @@ public class Solution
 
         for (int right = 0; right < s.Length; right++)
         {
-            if (charCount.ContainsKey(s[right]) && charCount[s[right]]>=left)
+            //2nd condition is to make sure the old duplicate index is outside the window or not.
+            //if yes then left should not jump. if no then left should jump to index+1
+            if (charCount.ContainsKey(s[right]) && charCount[s[right]]>=left) 
             {
                 left = charCount[s[right]] + 1;
             }
