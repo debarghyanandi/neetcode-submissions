b94bd2537b47c52c4867732c5277fd6f7e5b7514
Debarghya <debarghyanandi4@gmail.com>
2026-09-03 03:36:08 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int Search(int[] nums, int target) 
+    {
+        //My Solution
+        return Search(0, nums.Length-1 , target, nums);
+    }
+    
+    private int Search(int l, int r, int t, int[] nums)
+    {
+        if(l > r)
+            return -1;
+        
+        int mid = l + (r - l) / 2;
+        
+        if(nums[mid] == t)
+            return mid;
+        
+        if (t < nums[mid])
+            return Search(l, mid-1, t, nums);
+        
+        return Search(mid + 1, r, t, nums);
+    }
+}
