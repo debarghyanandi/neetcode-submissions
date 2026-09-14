62a96d4bcbce607386f56de3667efc259de3d1a2
Debarghya <debarghyanandi4@gmail.com>
2026-09-14 22:10:03 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public int[] FindOrder(int numCourses, int[][] prerequisites) {
+        //My solution
+        //Bfs Kanhs algo
+        //build the adjacency list
+        List<int>[] adj = new List<int>[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            adj[i] = new List<int>();
+        }
+        
+        int []inDeg = new int[numCourses];
+
+        //build the graph + indegree
+        foreach(int[] pair in prerequisites){
+            int course = pair[0];
+            int prereq = pair[1];
+            adj[prereq].Add(course);
+            inDeg[course]++;
+        }
+        
+
+        Queue<int> q = new Queue<int>();
+        //insert all the nodes whose indegree is 0;
+        for (int i = 0; i < numCourses; i++){
+            if(inDeg[i] == 0){
+                q.Enqueue(i);
+            }
+        }
+
+        List<int> topo = new List<int>();
+
+        while(q.Count > 0){
+            int node = q.Dequeue();
+            topo.Add(node);
+
+            //remove this node from its neightbours indegree
+            foreach(int nei in adj[node]){
+                inDeg[nei]--;
+                if(inDeg[nei] == 0)
+                q.Enqueue(nei);
+            }
+        }
+
+        return (topo.Count ==  numCourses) ? topo.ToArray() : Array.Empty<int>();
+    }
+}
